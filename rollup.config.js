e24081e79da67699aa42434236bebf8f4fdb8f18
ppotatoG <thgus2270@gmail.com>
2023-03-12 20:05:30 +0900

    Add: 5ecfb84

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
 import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: './src/js/sa.js',
-    output: {
-        file: './dist/sa.js',
-        format: 'iife',
-    },
-    plugins: [terser()],
+  input: './src/js/sa.js',
+  output: {
+    file: './dist/sa.js',
+    format: 'umd',
+    name: 'sa',
+  },
+  plugins: [resolve(), commonjs(), terser()],
 };
